1ce30ba8ce9bfd19efcad57e8bf98df0dc3cfbea
aldoshin <aldoshin@gmail.com>
2014-07-02 16:26:08 -0400

    backwash

--- Assigment_I/Percolation.java
+++ Assigment_I/Percolation.java
@@ -2,10 +2,10 @@ public class Percolation {
 
 	private static final int OPEN_STATUS = 1;
 	private int virtualTop = 0;
-	private int virtualBottom;
 	private int N;
 
 	private int[][] grid;
+	private boolean[] treeHasBottom;
 	private WeightedQuickUnionUF unionFind;
 
 	public Percolation(int N) {
@@ -13,9 +13,9 @@ public class Percolation {
 			throw new IllegalArgumentException();
 		}
 		this.N = N;
-		this.virtualBottom = (N * N) + 2;
+		this.treeHasBottom = new boolean[(N * N) + 1];
 		this.grid = new int[this.N][this.N];
-		this.unionFind = new WeightedQuickUnionUF(this.virtualBottom);
+		this.unionFind = new WeightedQuickUnionUF((N * N) + 1);
 	}
 
 	// open site (row i, column j) if it is not already
@@ -23,25 +23,47 @@ public class Percolation {
 		validateParms(i, j);
 		this.grid[i - 1][j - 1] = OPEN_STATUS;
 		int indexToOpen = xyTo1D(i, j);
-		if (i > 1) {
-			if (isOpen(i - 1, j)) {
-				unionFind.union(xyTo1D(i - 1, j), indexToOpen);
-			}
-		} else {
+		if (i == 1) {
 			unionFind.union(virtualTop, indexToOpen);
+		}
+		if (i == N) {
+			this.treeHasBottom[indexToOpen] = true;
 		}
 		if (i < this.N) {
 			if (isOpen(i + 1, j)) {
 				unionFind.union(xyTo1D(i + 1, j), indexToOpen);
+				percolatesUnion(indexToOpen, xyTo1D(i + 1, j));
 			}
-		} else {
-			unionFind.union(virtualBottom - 1, indexToOpen);
+		}
+		if (i > 1) {
+			if (isOpen(i - 1, j)) {
+				unionFind.union(xyTo1D(i - 1, j), indexToOpen);
+				percolatesUnion(indexToOpen, xyTo1D(i - 1, j));
+			}
+		}
+		if (i == 1 && i == N) {
+			percolatesUnion(virtualTop, indexToOpen);
 		}
 		if (j > 1 && isOpen(i, j - 1)) {
 			unionFind.union(xyTo1D(i, j - 1), indexToOpen);
 		}
 		if (j < this.N && isOpen(i, j + 1)) {
 			unionFind.union(xyTo1D(i, j + 1), indexToOpen);
+		}
+	}
+
+	private void percolatesUnion(int indexToOpen, int indexUnion) {
+		int treIndexToOpen = unionFind.find(indexToOpen);
+		int treeIndexUnion = unionFind.find(indexUnion);
+		if (this.treeHasBottom[indexToOpen]
+				|| this.treeHasBottom[treIndexToOpen]) {
+			this.treeHasBottom[treeIndexUnion] = true;
+			this.treeHasBottom[indexUnion] = true;
+		}
+		if (this.treeHasBottom[indexUnion]
+				|| this.treeHasBottom[treeIndexUnion]) {
+			this.treeHasBottom[treIndexToOpen] = true;
+			this.treeHasBottom[indexToOpen] = true;
 		}
 	}
 
@@ -59,11 +81,11 @@ public class Percolation {
 
 	// does the system percolate?
 	public boolean percolates() {
-		return unionFind.connected(virtualTop, virtualBottom - 1);
+		return this.treeHasBottom[unionFind.find(virtualTop)];
 	}
 
 	private int xyTo1D(int i, int j) {
-		return ((j - 1) + (this.N * (i - 1)));
+		return ((j - 1) + (this.N * (i - 1))) + 1;
 	}
 
 	private void validateParms(int i, int j) {
